1d3a588f3b45a6992ad4620c21b6d9769c0d6762
minjung Kim <ikwmjj@naver.com>
2022-01-14 10:16:26 +0900

    Add files via upload

---
+++ COS PRO 기출 1차/1차 2급 1_initial_code.py
@@ -0,0 +1,29 @@
+#You may use import as below.
+#import math
+
+def solution(shirt_size):
+    #Write code here.
+    answer = [0 for _ in range(6)]
+    
+    for i in shirt_size :
+        if i == "XS" : 
+            answer[0] += 1
+        elif i == "S" :
+            answer[1] += 1
+        elif i == "M" :
+            answer[2] += 1
+        elif i == "L" :
+            answer[3]+=1
+        elif i == "XL" :
+            answer[4]+=1
+        else :
+            answer[5] += 1
+            
+    return answer
+
+#The following is code to output testcase.
+shirt_size = ["XS", "S", "L", "L", "XL", "S"]
+ret = solution(shirt_size);
+
+#Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
